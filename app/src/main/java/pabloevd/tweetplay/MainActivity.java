a54f95908640ea6934be6c35a12c4bf2306daca5
pabloevd950 <pabloevd950@gmail.com>
2017-02-26 23:13:17 -0600

    Setup Spotify auth and player. Started main acitvity view for playlists

--- app/src/main/java/pabloevd/tweetplay/MainActivity.java
+++ app/src/main/java/pabloevd/tweetplay/MainActivity.java
@@ -1,14 +1,208 @@
+
+
 package pabloevd.tweetplay;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.widget.ArrayAdapter;
+        import android.widget.ListView;
+        import android.widget.AdapterView;
+        import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+        import com.google.android.gms.appindexing.Action;
+        import com.google.android.gms.appindexing.AppIndex;
+        import com.google.android.gms.appindexing.Thing;
+        import com.google.android.gms.common.api.GoogleApiClient;
+        import com.spotify.sdk.android.authentication.AuthenticationClient;
+        import com.spotify.sdk.android.authentication.AuthenticationRequest;
+        import com.spotify.sdk.android.authentication.AuthenticationResponse;
+        import com.spotify.sdk.android.player.Config;
+        import com.spotify.sdk.android.player.ConnectionStateCallback;
+        import com.spotify.sdk.android.player.Error;
+        import com.spotify.sdk.android.player.Player;
+        import com.spotify.sdk.android.player.PlayerEvent;
+        import com.spotify.sdk.android.player.Spotify;
+        import com.spotify.sdk.android.player.SpotifyPlayer;
+        import java.util.ArrayList;
+
+
+public class MainActivity extends Activity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback{
+    //TODO: Create list of playlist objects
+    ListView playLList;
+    String items;
+    private static final String CLIENT_ID = "42e4cf334d044ee3b93e7dcf12a83b3f";
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";
+
+    // Request code that will be used to verify if the result comes from correct activity
+    // Can be any integer
+    private static final int REQUEST_CODE = 1337;
+
+    private Player mPlayer;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        populatePlayListView();
         setContentView(R.layout.activity_main);
-        //Hello
+
+        String[] myItems = {"Party1", "Party2", "Party3", "Part4"}; // Build Adapter
+        ArrayAdapter<String> playListAdapter = new ArrayAdapter<String>(this, R.layout.the_playlist, myItems);
+        ListView playlists = (ListView)findViewById(R.id.playLList);
+        playlists.setAdapter(playListAdapter);
+
+
+//        setContentView(R.layout.activity_main);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void populatePlayListView(){
+        //Create list of items
+
+        //Build adapter
+
+        //Set up list view
+
+        String[] myItems = {"Party1", "Party2", "Party3", "Part4"}; // Build Adapter
+        ArrayAdapter<String> playListAdapter = new ArrayAdapter<String>(this, R.layout.activity_main, myItems);
+        ListView playlists = (ListView)findViewById(R.id.playLList);
+        playlists.setAdapter(playListAdapter);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addNotificationCallback(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        // VERY IMPORTANT! This must always be called or else you will leak resources
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+
+        mPlayer.playUri(null, "spotify:track:3bnVBN67NBEzedqQuWrpP4", 0, 0);
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
---
+++ app/src/main/java/pabloevd/tweetplay/NowPlayingActivity.java
@@ -0,0 +1,8 @@
+package pabloevd.tweetplay;
+
+/**
+ * Created by pablovalero on 2/26/17.
+ */
+
+public class NowPlayingActivity {
+}
