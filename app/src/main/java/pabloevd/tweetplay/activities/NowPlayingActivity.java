d08039fdbdf57525fd6dee1cca3e4c042ccfcd0f
pabloevd950 <pabloevd950@gmail.com>
2017-03-11 12:40:20 -0600

    Toggle between song and queue fragment. Use repeat button for now

--- app/src/main/java/pabloevd/tweetplay/activities/NowPlayingActivity.java
+++ app/src/main/java/pabloevd/tweetplay/activities/NowPlayingActivity.java
@@ -12,9 +12,11 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import pabloevd.tweetplay.R;
+import pabloevd.tweetplay.fragments.QueueFragment;
 import pabloevd.tweetplay.fragments.SongFragment;
 
 /** TODO
@@ -22,14 +24,41 @@ import pabloevd.tweetplay.fragments.SongFragment;
  * and to display the queue list
  */
 
-public class NowPlayingActivity extends AppCompatActivity implements View.OnClickListener, SongFragment.OnFragmentInteractionListener {
+public class NowPlayingActivity extends AppCompatActivity implements View.OnClickListener, SongFragment.OnFragmentInteractionListener, QueueFragment.OnFragmentInteractionListener {
+    private ImageButton playButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private ImageButton shuffleButton;
+    private ImageButton changeFragButton;
+    private int viewNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        viewNum =1;
         setContentView(R.layout.activity_nowplaying);
-        Fragment fragment = new SongFragment();
+        final Fragment fragment = new SongFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
+
+        changeFragButton = (ImageButton) findViewById(R.id.repeatButton);
+        changeFragButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewNum == 1) {
+                    viewNum = 2;
+                    final Fragment fragment2 = new QueueFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment2, fragment2.getClass().getSimpleName()).addToBackStack(null).commit();
+                }
+                else{
+                    viewNum =1;
+                    final Fragment fragment = new SongFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
+
+                }
+            }
+        });
+
+
 
     }
 
